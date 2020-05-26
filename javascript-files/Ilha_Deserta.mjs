16c7961f6af3b92695ddf3068c6b48b6396e0ebc
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-05-26 17:30:25 -0300

    - Dever terminado

--- javascript-files/Ilha_Deserta.mjs
+++ javascript-files/Ilha_Deserta.mjs
@@ -1,3 +1,11 @@
-var item1 = ("Uma faca, ")
-var item2 = ("uma ")
-var item3 = ("")
+var item1 = ("Um facão");
+var item2 = ("uma garrafa de água");
+var item3 = ("uma pederneira");
+
+function leveiPraIlha(materiais) {
+
+  alert("Eu levaria para uma ilha deserta: " + item1 + ", " + item2 + " e " + item3 + ".");
+
+}
+
+document.write('<button type="button" onclick=" leveiPraIlha()">Levei Pra ilha</button>');
