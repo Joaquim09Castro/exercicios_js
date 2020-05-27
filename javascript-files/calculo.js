b4da783b751754414768a843001879b5fcf27d06
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-05-26 21:36:12 -0300

    - Dever esquecido feito

---
+++ javascript-files/calculo.js
@@ -0,0 +1,25 @@
+
+var numero = (8);
+var nome = ("Fulâno");
+
+function dobro() {
+
+  document.write( numero * 2 + "<br>");
+
+}
+
+function metade() {
+
+  document.write( numero / 2 + "<br>");
+
+}
+
+function saudacao() {
+
+  document.write( "olá " + nome + "<br>");
+
+}
+
+dobro();
+metade();
+saudacao();
